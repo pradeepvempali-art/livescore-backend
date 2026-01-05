5a640809dde9089c8d2f053a685062b9225c49d8
pradeep <pradeep@example.com>
2026-01-05 11:18:19 +0530

    main

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // frontend URL
+    origin: ['http://localhost:3001', 'https://livescore-frontend.vercel.app'],
     credentials: true,
   });
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 void bootstrap();
