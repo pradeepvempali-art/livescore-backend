129fb4d7900167c8f049716176eb50ecbe8d9edc
pradeep <pradeep@example.com>
2026-01-05 15:12:05 +0530

    are

--- src/player-stats/player-stats.module.ts
+++ src/player-stats/player-stats.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PlayerStatsService } from './player-stats.service';
 import { PlayerStatsController } from './player-stats.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [PlayerStatsController],
